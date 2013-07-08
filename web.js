5ebb07214f046adea5f38bd9665e395b8e8ec505
Delius <pawel@infonetlive.com>
2013-07-08 21:49:26 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -2,15 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-/*app.get('/', function(request, response) {
+app.get('/', function(request, response) {
     var buffer = new Buffer("Hello World from index.html", 25);
     var data =  fs.readFileSync("index.html", "utf-8");
-	response.send((resultOfreadFileSync);
+	response.send(resultOfreadFileSync);
 });
-*/
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
