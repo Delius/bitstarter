814ca2d2bcf4f0081d02cf5347eb98eb5487877d
Delius <pawel@infonetlive.com>
2013-07-08 21:58:19 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -2,7 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
+    var fs = require('fs');
     var buffer = new Buffer("Hello World from index.html", 25);
     var data =  fs.readFileSync("index.html", "utf-8");
 	response.send(resultOfreadFileSync);
