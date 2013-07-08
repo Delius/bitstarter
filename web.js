d6dc0ed4f5c65a2ab5d444f06e4356bccc81df08
Delius <pawel@infonetlive.com>
2013-07-08 20:34:28 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
-var express = require('express');
+//var express = require('express');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
+
+var buffer = new Buffer("Hello World from index.html", 25);
+var data = fs.readFileSync("index.html"), buffer.toString("utf-8"));
+response.send(data);
+  });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
